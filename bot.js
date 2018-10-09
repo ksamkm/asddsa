b96caef7a40260eea1dfa7a8a448bb66e1b88f86
ksamkm <43929021+ksamkm@users.noreply.github.com>
2018-10-09 17:56:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = "$"
+const prefix = "-"
 
 const UserBlocked = new Set();
 
@@ -59,6 +59,161 @@ client.user.setGame(`Nothing҉`,"http://twitch.tv/Mohamed192837465")
   console.log('')
 
 });
+
+
+
+
+
+const bannedwords = [//Alpha Codes
+  "#credit",//Alpha Codes
+  "#profile",//Alpha Codes
+//Alpha Codes
+  "#rep",//Alpha Codes
+  "#top",//Alpha Codes
+  "%level",//Alpha Codes
+  "%تقديم",//Alpha Codes
+  "-play",//Alpha Codes
+  "-stop",//Alpha Codes
+  "-p",//Alpha Codes
+  "-s",//Alpha Codes
+  "!invites",//Alpha Codes
+  "!top",//Alpha Codes
+  "G.play",//Alpha Codes
+  "G.stop",
+  "G.skip",
+  "-skip"//Alpha Codes
+//Alpha Codes
+
+]
+client.on('message', message => {
+  var Muted = message.guild.roles.find("name", "muted");
+  var warn = message.guild.roles.find("name", "warn");
+  if(bannedwords.some(word => message.content.includes(word))) {
+  if(message.channel.id !== 'id chat') return;
+  if (message.author.bot) return;
+  if(message.member.roles.has(warn)) return;
+  if(!message.member.roles.has(warn.id)) {
+  message.member.addRole(warn)
+  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
+  }
+  if(message.member.roles.has(warn.id)) {
+      message.member.addRole(Muted)
+      message.member.removeRole(warn)
+      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
+  }
+  }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
