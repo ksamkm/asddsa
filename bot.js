31f652fea3843bacc8edd03641a5d2c991ded8b0
ksamkm <43929021+ksamkm@users.noreply.github.com>
2018-10-09 17:46:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,66 @@
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
-import re
-from typing import List
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const prefix = "$"
+
+const UserBlocked = new Set();
 
 
-client = commands.Bot(command_prefix = ".")
 
-@client.event
-async def on_ready():
-   print("Bot Opend")
-   
- 
- 
- 
- 
-client.run("Token")
+
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
+client.user.setGame(`Nothing҉`,"http://twitch.tv/Mohamed192837465")
+
+  console.log('')
+
+  console.log('')
+
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+
+  console.log(`[Start] ${new Date()}`);
+
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+
+  console.log('')
+
+  console.log('╔[════════════════════════════════════]╗');
+
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+
+  console.log('')
+
+  console.log('Informations :')
+
+  console.log('')
+
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+
+  console.log(`Users! [ " ${client.users.size} " ]`);
+
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+
+  console.log('╚[════════════════════════════════════]╝')
+
+  console.log('')
+
+  console.log('╔[════════════]╗')
+
+  console.log(' Bot Is Online')
+
+  console.log('╚[════════════]╝')
+
+  console.log('')
+
+  console.log('')
+
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN);
