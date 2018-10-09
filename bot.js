13460eee0d7b66eae5ccf310a4005ba536ce38f5
ksamkm <43929021+ksamkm@users.noreply.github.com>
2018-10-09 18:20:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,28 @@ client.on('message', message=> {
 
 
 
-
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('+bc')) {
+          if (!args[0]) {
+message.channel.send("**+bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
 
 
 
