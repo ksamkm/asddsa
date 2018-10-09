f4f62b53577a6883a35fcdb46f3aa8dedd439e94
ksamkm <43929021+ksamkm@users.noreply.github.com>
2018-10-09 13:55:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,44 @@
-var Discord = require('discord.io');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
 
- 
-
-var bot = new Discord.Client({
-
-    token: "",
-
-    autorun: true
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
- 
-
-bot.on('ready', function() {
-
-    console.log('Logged in as %s - %s\n', bot.username, bot.id);
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
- 
-
-bot.on('message', function(user, userID, channelID, message, event) {
-
-    if (message === "ping") {
-
-        bot.sendMessage({
-
-            to: channelID,
-
-            message: "pong"
-
-        });
-
-    }
-
+cconst swearWords = ["كل زق", "كس امك", "يامطي", "", "", "", "", "", "", "", "", ""]; 
+  if( swearWords.some(word => message.content.includes(word)) ) {
+    message.reply("مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ").then(sentMessage =>{
+      sentMessage.delete(20000)
+    })
+    message.delete(3000)
+    client.channels.get('ايدي الروم').send(message.author.toString() + "استخدم كلام لا يليق ~")
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('token');
