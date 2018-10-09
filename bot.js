4ae0628931b1168d5ca3319891f6a17f377b00d3
ksamkm <43929021+ksamkm@users.noreply.github.com>
2018-10-09 19:08:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,20 @@ client.on('message', function(msg) {
 
 
 
+client.on('message', msg => {
 
+    if (msg.content == '!تعال ههنا يابوت') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("498824991287017472").join();
+    });
 
 
 
